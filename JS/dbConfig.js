2267a6d7a009fdc03e8ae1be008f22b22d7a2cd1
Saki <105495123+paldooon@users.noreply.github.com>
2022-10-17 11:56:51 +0900

    Update dbConfig.js

--- JS/dbConfig.js
+++ JS/dbConfig.js
@@ -1,5 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const config = {
+const firebaseConfig = {
   apiKey: "AIzaSyCs14YKX_3Yl-xoPF-z8HyKbpgMVlvteyw",
   authDomain: "chatdatabase-16886.firebaseapp.com",
   projectId: "chatdatabase-16886",
@@ -8,4 +15,7 @@ const config = {
   appId: "1:206742703510:web:d027b28ec905be4ef161b7",
   measurementId: "G-YGGXGRX05K"
 };
-firebase.initializeApp(config);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
